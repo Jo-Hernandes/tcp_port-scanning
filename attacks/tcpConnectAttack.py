f33f2cad4d3db95b11f41ece2d781895fb221bd8
osboxes.org <joohernandes@gmail.com>
2022-06-21 10:02:01 -0400

    adjustments on seq and ack number

--- attacks/tcpConnectAttack.py
+++ attacks/tcpConnectAttack.py
@@ -6,7 +6,7 @@ def __onTcpReceived(dstHost, srcHost, tcpPacket):
     if (tcpPacket['flag_ack'] == 1 and tcpPacket['flag_syn'] == 1):
         seq = tcpPacket['sequence'] + 1 
         ack = tcpPacket['ack'] + 1
-        eUtils.sendeth(getTcpPacket(dstHost, srcHost, ACK, seqNumber=seq, ackNumber=ack), srcHost.interface, srcHost.port)               
+        eUtils.sendeth(getTcpPacket(dstHost, srcHost, ACK, seqNumber=ack, ackNumber=seq), srcHost.interface, srcHost.port)
         return True
     else :
         return False
