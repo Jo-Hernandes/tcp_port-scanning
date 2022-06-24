bdb0a3131d9eb3e9b3851f0d317eb66beea1741d
osboxes.org <joohernandes@gmail.com>
2022-06-24 17:32:53 -0400

    removed print

--- attacks/tcpConnectAttack.py
+++ attacks/tcpConnectAttack.py
@@ -2,12 +2,10 @@ from attacks.defaultAttack import *
 from domain.tcpHeaderFlag import SYN, ACK, FIN
 
 def __onTcpReceived(dstHost, srcHost, tcpPacket):
-    print(tcpPacket)
     if (tcpPacket['flag_ack'] == 1 and tcpPacket['flag_syn'] == 1):
         seq = tcpPacket['sequence'] + 1 
         ack = tcpPacket['ack'] + 1
         eUtils.sendeth(getTcpPacket(dstHost, srcHost, ACK, seqNumber=ack, ackNumber=seq), srcHost.interface, srcHost.port)
-
         return True
     else :
         return False
